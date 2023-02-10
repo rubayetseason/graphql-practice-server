003e3547f07efc2c3244abedd3c69c4e2a0d038e
rubayet-islam <season.ss2001@gmail.com>
2023-02-10 20:35:37 +0600

    created mutation schema

--- index.js
+++ index.js
@@ -28,15 +28,37 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     getAllUsers: {
       type: new GraphQLList(UserType),
-      args: {
-        id: {
-          type: GraphQLInt,
-        },
+      args: { id: { type: GraphQLInt } },
+      resolve(parent, arg) {
+        return userData;
       },
     },
   },
 });
-const Mutation = "mutation";
+const Mutation = new GraphQLObjectType({
+  nameL: "Mutation",
+  fields: {
+    createUser: {
+      type: UserType,
+      args: {
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        userData.push({
+          id: userData.length + 1,
+          firstName: args.firstName,
+          lastName: args.lastName,
+          email: args.email,
+          password: args.password,
+        });
+        return args;
+      },
+    },
+  },
+});
 
 const schema = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
 
