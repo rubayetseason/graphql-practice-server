9732935e29b34a16cf05ef9758686c507f25748e
rubayet-islam <season.ss2001@gmail.com>
2023-02-10 18:26:47 +0600

    created a schema to get all the user data, still have to add the resolve to the schema function

--- index.js
+++ index.js
@@ -2,26 +2,48 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 const userData = require("./DATA.json");
-//got the fake data from the json folder
-const graphql = require("graphql");
-//installed graphql and express-graphql, then required it
+const {
+  graphql,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+} = require("graphql");
 const { graphqlHTTP } = require("express-graphql");
-//required this to create a graphql server
 
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    id: { type: GraphQLInt },
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+  }),
+});
 
-//this is a basic schema we created for the server
-const RootQuery = "query";
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    getAllUsers: {
+      type: new GraphQLList(UserType),
+      args: {
+        id: {
+          type: GraphQLInt,
+        },
+      },
+    },
+  },
+});
 const Mutation = "mutation";
 
 const schema = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
-//this is a basic schema we created for the server
 
-//then as step-2 we create a basic graphql server down here
 app.use(
   "/graphql",
   graphqlHTTP({
     schema,
-    //here we need to create a schema in the third step
     graphiql: true,
   })
 );
