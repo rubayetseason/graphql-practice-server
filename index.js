21d1b1143ded30378d5e69d7cdd3336ea570e098
rubayet-islam <season.ss2001@gmail.com>
2023-02-16 14:25:47 +0600

    cors added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 5000;
 const { graphqlHTTP } = require("express-graphql");
+app.use(cors());
 
 const schema = require("./Schemas/schema");
 
